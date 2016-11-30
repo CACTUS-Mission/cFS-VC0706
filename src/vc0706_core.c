115a432e87dc0efdb73d5afdbe4e6d65bc1437c5
Ezra Brooks <ezra.m.brooks@gmail.com>
2016-11-29 22:06:37 -0500

    Document all the camera command codes

--- src/vc0706_core.c
+++ src/vc0706_core.c
@@ -13,7 +13,7 @@ extern struct led_t led; /**< LED instance from vc0706.c */
 
 /**
  * Initializes the cameras' serial interfaces.
- * \param cam[in,out] - A pointer to the Camera structure to initialize
+ * \param[in,out] cam - A pointer to the Camera structure to initialize
  * \param ttyInterface - The serial (TX/RX) interface the camera is plugged into. In the case of the CACTUS-1 Pi-Sat boards, 0 and 1 are valid.
  */
 int init(Camera_t *cam, uint8 ttyInterface)
@@ -173,7 +173,7 @@ int getVersion(Camera_t *cam)
 
 /**
  * Enables or disables motion detection on the specified camera
- * \param cam[in,out] - A pointer to the camera to set the motion detection flag on
+ * \param[in,out] cam - A pointer to the camera to set the motion detection flag on
  * \param flag - Enable (1) or disable (0) motion detection.
  */
 void setMotionDetect(Camera_t *cam, bool flag)
@@ -194,6 +194,7 @@ void setMotionDetect(Camera_t *cam, bool flag)
 /**
  * Sends a command over serial to the specified camera.
  * \param cam - A pointer to the camera to command
+ * \param cmd - The type of command to send
  * \param args - An array of the codes to write to the interface
  * \param argLen - The length of the array
  */
@@ -211,7 +212,7 @@ void sendCommand(Camera_t *cam, uint8_t cmd, uint8_t args[], uint8_t argLen)
 
 /**
  * Takes a picture and writes it to disk.
- * \param cam[in,out] - A pointer to the camera to take a picture with
+ * \param[in,out] cam - A pointer to the camera to take a picture with
  * \param file_path - The name of the file to save to
  */
 char *takePicture(Camera_t *cam, char *file_path)
